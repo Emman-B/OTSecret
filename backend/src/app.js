2ab4b77d36636006788093df158399209e8ea3ab
Emmanuel Butor <emmanuelbutor.mehs@gmail.com>
2021-08-23 22:37:21 -0700

    Set up the basic backend with default route

---
+++ backend/src/app.js
@@ -0,0 +1,50 @@
+// set up dotenv
+require('dotenv').config();
+
+
+const express = require('express');
+const app = express(); // app is exported
+
+
+// == Setting up api validation ==
+const OpenApiValidator = require('express-openapi-validator');
+const swaggerUi = require('swagger-ui-express'); // UI API testing
+const YAML = require('js-yaml');
+const fs = require('fs');
+const apiSpec = YAML.load(fs.readFileSync('src/api/openapi.yaml')); // load the api spec
+
+/**
+ * Middleware Setup
+ */
+// set up express json parser
+app.use(express.json());
+
+// == Setting up api validation middleware ==
+// this sets up the api testing GUI
+app.use('/v1/api-docs', swaggerUi.serve, swaggerUi.setup(apiSpec));
+// this sets up the api validation middleware
+app.use(
+    OpenApiValidator.middleware({
+        apiSpec: apiSpec,
+        validateRequests: true,
+        validateResponses: true,
+    })
+);
+// this is the error handler
+app.use((err, req, res, next) => {
+    // format error
+    res.status(err.status || 500).json({
+        message: err.message,
+        errors: err.errors,
+    });
+});
+
+/**
+ * Paths
+ */
+// Default path to test if server is on
+app.get('/v1/', (req, res) => {
+    res.status(200).send('Hello world!');
+});
+
+module.exports = app;
---
+++ backend/src/server.js
@@ -0,0 +1,8 @@
+// This is for starting the server
+
+const app = require('./app');
+
+// Starts the server
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${process.env.PORT}`);
+});
